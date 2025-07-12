7d0ad8283fd25e64a10a05285a74f001ff60c83e
Rahil670 <rahilahmad22744@gmail.com>
2025-07-12 13:10:11 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,18 @@
+RippleX Firebase-Ready Setup
+
+1. Firebase Hosting:
+   - Run `firebase login`
+   - Run `firebase init` (choose Hosting and select existing project: ripplex-c114a)
+   - Run `firebase deploy`
+
+2. Firebase Auth Enabled:
+   - Google
+   - Email/Password
+   - Phone (OTP)
+
+3. Files:
+   - index.html: Main App
+   - firebase.js: Firebase config
+   - firebase.json: Hosting config
+
+You're ready to deploy!
---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Firebase Configuration for RippleX
+const firebaseConfig = {
+  apiKey: "AIzaSyD9W3pmTXi84IKsZfi8UmGhOlWZ_8dA8eU",
+  authDomain: "ripplex-c114a.firebaseapp.com",
+  projectId: "ripplex-c114a",
+  storageBucket: "ripplex-c114a.firebasestorage.app",
+  messagingSenderId: "1065847446781",
+  appId: "1:1065847446781:web:1d0316d1438764e0993294",
+  measurementId: "G-ZRCB4QQMM5"
+};
+firebase.initializeApp(firebaseConfig);
